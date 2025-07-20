fd18fe4322c4b6a27674260ed378c1b5c09f32c2
Emy Santo <142574788+Emysanto@users.noreply.github.com>
2025-07-20 15:33:37 +0530

    Create day1.py
    Basics in Basic

---
+++ day1.py
@@ -0,0 +1,54 @@
+# This is how a single-line comment is plotted
+'''this is how multiline comments are plotted '''
+
+# An integer assignment 
+age = 45                        
+# A floating point 
+salary = 1456.8            
+   
+# A string   
+name = "John"             
+   
+print(age) 
+print(salary) 
+print(name)
+
+# Examples of Arithmetic Operators 
+a = 9
+b = 4
+   
+# Addition of numbers 
+add = a + b 
+# Subtraction of numbers  
+sub = a-b 
+# Multiplication of numbers  
+mul = a * b 
+# Division(float) of number  
+div1 = a / b 
+# Division(floor) of number  
+div2 = a // b 
+# Modulo of both number 
+mod = a % b 
+   
+# print results 
+print(add) 
+print(sub) 
+print(mul) 
+print(div1) 
+print(div2) 
+print(mod)
+
+
+# Examples of Logical Operator 
+a = True
+b = False
+   
+# Print a and b are False 
+print(a and b) 
+   
+# Print a or b is True 
+print(a or b) 
+   
+# Print not a is False 
+print(not a)
+
